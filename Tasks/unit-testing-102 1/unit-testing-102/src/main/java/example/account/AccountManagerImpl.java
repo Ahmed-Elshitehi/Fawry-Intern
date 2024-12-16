3ac6838d9f633faf4d7f59ba393327e5d4f89736
Ahmed-Elshitehi <elshitehi1@gmail.com>
2024-12-16 22:45:18 +0200

    Account Manager Test Done

--- Tasks/unit-testing-102 1/unit-testing-102/src/main/java/example/account/AccountManagerImpl.java
+++ Tasks/unit-testing-102 1/unit-testing-102/src/main/java/example/account/AccountManagerImpl.java
@@ -4,17 +4,24 @@ public class AccountManagerImpl implements AccountManager {
     private static final int MAX_CREDIT = 1000;
     @Override
     public void deposit(Customer customer, int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("amount can't be negative");
+        }
         customer.setBalance(customer.getBalance() + amount);
     }
 
     @Override
     public String withdraw(Customer customer, int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("amount can't be negative");
+        }
+
         int expectedBalance = customer.getBalance() - amount;
 
         if (expectedBalance < 0) {
             if (!customer.isCreditAllowed()) {
                 return "insufficient account balance";
-            } else if (expectedBalance > MAX_CREDIT && !customer.isVip()) {
+            } else if (expectedBalance < -1 * MAX_CREDIT && !customer.isVip()) {
                 return "maximum credit exceeded";
             }
         }
